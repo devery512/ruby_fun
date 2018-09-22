dcb1eb1be5940726e33749b12b1f494584f2e365
Garrian Brown <garrianbrown@gmail.com>
2018-09-21 19:34:52 -0500

    added comments to help explain each line of code used to make this work.

--- histogram.rb
+++ histogram.rb
@@ -1,14 +1,14 @@
 puts = "Please enter a sentence you would like to anaylize below."
 text = gets.chomp
-
+#Building the Words Array
 words = text.split
-
+#Creating the Frequencies Hash
 frequencies = Hash.new(0)
-
+#Iterating Over the Array
 words.each {|words| frequencies[words] += 1}
-
+#Sorting the Hash
 frequencies = frequencies.sort_by{ |words, count| count}
-
+#Printing out the result
 frequencies.each do |words, count|
 	puts "#{words} #{count.to_s}"	
 end
